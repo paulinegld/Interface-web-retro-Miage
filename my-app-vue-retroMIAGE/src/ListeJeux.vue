8feb09126751c581c214b3dfc33b90ec6e845db5
Pastor-Loic <44533866+LoPastor@users.noreply.github.com>
2019-04-04 16:51:34 +0200

    Add files via upload

--- my-app-vue-retroMIAGE/src/ListeJeux.vue
+++ my-app-vue-retroMIAGE/src/ListeJeux.vue
@@ -9,7 +9,8 @@
 
       <td>{{ props.item.name }}</td>
       <td class="text-xs-left">{{ props.item.console}}</td>
-
+      <td class="imageDisplay" v-if="props.item.image!=''"><img v-bind:src="props.item.image" width=200></img></td>
+      <td v-else><button>Ajouter une image</button></td>
 
     </template>
   </v-data-table>
@@ -27,32 +28,43 @@
               sortable: true,
               value: 'name'
             },
-            {text: 'Console', value: 'console', sortable: true}
+            {text: 'Console', value: 'console', sortable: true},
+            {
+              text: "Image du jeu",
+              align: 'center',
+              sortable:true
+            }
           ],
           listejeux: [{
-            name: 'Super Mario Bross',
-            console: 'Wii',
+            name: 'Super Mario Bros',
+            console: 'NES',
+            image:"src/images/marioB.jpg"
           },
             {
-              name: 'Mario Kart',
+              name: 'Mario Kart Wii',
               console: 'Wii',
+              image:""
             },
             {
               name: 'Nintendogs',
               console: 'Nintendo DS',
+              image:""
             },
             {
-              name: 'Animal Crossing',
-              console: 'Nintendo DS',
+              name: 'Terranigma',
+              console: 'Super Nintendo',
+              image:"src/images/terranigma.png"
             },
             {
-              name: 'Metroid',
+              name: 'Metroid Prime Hunters',
               console: 'Nintendo DS',
+              image:""
             }
           ]
         }
       }
     }
+
 </script>
 
 <style scoped>
